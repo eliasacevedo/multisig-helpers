c0a7097545ba77165cb8a13a497d780fd7b5ff6e
Elias Acevedo <eliasjoseacevedo61@gmail.com>
2022-05-19 00:05:51 -0400

    adding variables file

---
+++ variables.sh
@@ -0,0 +1,14 @@
+# Blockchain configuration
+Binary=starsd
+RPCEndpoint=https://rpc.stargaze-apis.com/
+ChainId=stargaze-1
+
+# Wallets configuration
+MultisigWalletName=multi
+NormalWalletName=test2
+NormalWalletSecp256k1Address=AtObiFVE4s+9+RX5SP8TN9r2mxpoaT4eGj9CJfK7VRzN
+
+# Ledger configuration
+IsNormalAccountUsingLedger=true
+DerivationPathId=0
+
